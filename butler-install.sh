251d58c177bcff5d2157375f36f7e50526bcbc52
GarretSidzaka <ringworldwu@gmail.com>
2024-11-30 15:24:15 -0700

    Update butler-install.sh

--- butler-install.sh
+++ butler-install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # prompt user for the absolute path to the docker compose yaml
+read -p
+
 
 #create a startup cron job https://stackoverflow.com/questions/878600/how-to-create-a-cron-job-using-bash-automatically-without-the-interactive-editor
 
