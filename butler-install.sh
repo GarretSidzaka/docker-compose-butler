c4e57800a21b8daf99971c011d91f5c1ef298d11
GarretSidzaka <ringworldwu@gmail.com>
2024-11-30 13:47:36 -0700

    Create butler-install.sh

---
+++ butler-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# prompt user for the absolute path to the docker compose yaml
+
+#create a startup cron job https://stackoverflow.com/questions/878600/how-to-create-a-cron-job-using-bash-automatically-without-the-interactive-editor
+
+
+#create a shutdown stop script https://www.golinuxcloud.com/run-script-with-systemd-before-shutdown-linux/
+
+#cleanup and exit
